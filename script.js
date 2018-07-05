f0a8e94383cfcb1403ec5af7996322f8056082be
Mariya <mariya.ave@mail.ru>
2018-07-06 00:48:24 +0300

    adaptive

--- script.js
+++ script.js
@@ -2,11 +2,14 @@
 // у нажатой клавиши забираем data-id
 // в div posts  отображаем div с классом из data-id
 
-const posts = document.getElementById("posts");
+
 const toggles = document.getElementById("toggles"); // Element
-const allPosts = posts.getElementsByClassName("post"); //HtmlCollection
+
+
 
 toggles.addEventListener('click', function (e) {
+    const posts = document.getElementById("posts");
+    const allPosts = posts.getElementsByClassName("post"); //HtmlCollection
     const target = e.target || e.srcElement;
     const condition = target.dataset.id;
     if (typeof(condition) === "undefined")
@@ -26,3 +29,31 @@ toggles.addEventListener('click', function (e) {
         }
     }
 });
+
+const main_menu_id = document.getElementById("main_menu_id");
+const menu_show = document.getElementById("menu_show");
+// const all_items = main_menu_id.getElementsByClassName("menu-item");
+
+
+// menu_show.addEventListener('click', function () {
+//     if (window.innerWidth <= 768) {
+//         if (main_menu_id.style.display === "none" || main_menu_id.style.display === "") {
+//
+//             //     main_menu_id.style.display="flex";
+//             // } else {
+//             main_menu_id.style.display = "block";
+//         } else {
+//             main_menu_id.style.display = "none";
+//         }
+//     }
+//
+// });
+// menu_show.onclick = function() {
+//     if (main_menu_id.style.display === "none") {
+//         main_menu_id.style.display = "block";
+//     } else {
+//         console.log(123);
+//         main_menu_id.style.display = "none";
+//     }
+//
+// };
